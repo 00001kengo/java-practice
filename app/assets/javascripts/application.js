84296adb4131fa342a11ba32c288052002196d14
00001kengo <wadakengo579@gmail.com>
2022-03-26 12:26:28 +0000

    java practice1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+let promptStr = prompt('何か好きな文字を入力してください。');
+
+alert(promptStr);
