fd8b89e73a970fe098de9daea3b88c5a1103c642
00001kengo <wadakengo579@gmail.com>
2022-04-14 14:31:26 +0000

    java 3-5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,3 +79,30 @@ $class Main {
     System.out.println("奇数の和は" + oddSum + "です");
     System.out.println("偶数の和は" + evenSum + "です");
   }
+  
+  
+  $class Main {
+  public static void main(String[] args) {
+    // fullNameメソッドの結果を変数nameに代入してください
+    String name = fullName("Kate","Jones");
+    
+    
+    // printDataの引数を書き換えてください
+    printData(name, 27);
+    
+    // こちらは書き換えないでください
+    printData("John Christopher Smith", 65);
+    
+  }
+
+  public static void printData(String name, int age) {
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+  }
+
+  // fullNameメソッドを定義してください
+  public static String fullName(String firstName,String lastName) {
+    return firstName + " " + lastName;
+  }
+  
+}
