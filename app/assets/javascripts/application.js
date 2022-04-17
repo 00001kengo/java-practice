5898a2d42ed70635760fc09c3292a257a4203c43
00001kengo <wadakengo579@gmail.com>
2022-04-17 15:19:06 +0000

    java 3-12

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -106,3 +106,15 @@ $class Main {
   }
   
 }
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("名前：");
+    String name = scanner.next();
+    
+    System.out.println("名前は" + name + "です");
+  }
+}
